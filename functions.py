3ef6aa57a8606a0a65077d66c8db6e4829d11479
Sadab-debug <1998prova@gmail.com>
2024-06-02 18:30:55 +0600

    Added Admin Account

--- functions.py
+++ functions.py
@@ -1,4 +1,8 @@
+from tkinter import filedialog
 import tkinter.messagebox as messagebox
+from PIL import Image, ImageTk
+import json
+
 
 #border effect on hover
 def onEnter(event, var):
@@ -19,8 +23,35 @@ def showCopyrightClaim(ctk, frame_name):
     )
     copyright_claim.place(relx=0.95, rely=0.95, anchor="se")
 
+
 def showErrorMessage(message):
     messagebox.showerror("Error!", message)
 
 def showInfo(message):
     messagebox.showinfo("Info", message)
+
+
+def uploadImage(self):
+    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg;*.jpeg;*.png")])
+    if file_path:
+        try:
+            with open('admin_id.json', 'r') as f:
+                data = json.load(f)
+
+            data['profile_pic'] = file_path  # Update the profile_pic path
+
+            with open('admin_id.json', 'w') as f:
+                json.dump(data, f, indent=4)
+
+            image = Image.open(file_path)
+            image = image.resize((200, 200), Image.Resampling.LANCZOS)
+            self.profile_image = ImageTk.PhotoImage(image)
+
+            # Update the label to show the profile picture
+            self.profile_picture_label.configure(image=self.profile_image, text="")
+
+        except FileNotFoundError:
+            showErrorMessage("The admin_id.json file does not exist.")
+
+        except json.JSONDecodeError:
+            showErrorMessage("Error reading the admin_id.json file.")
